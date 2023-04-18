84c5ce0df2bc25e8444a1685406be7a85ccf8c46
Ritesh9211 <riteshsaj8958@gmail.com>
2023-04-18 16:29:07 +0530

    Changes in URL

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,11 +9,13 @@ import { useLocation } from "react-router";
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
+  const [loading,setLoading] = useState(false)
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get("https://blogbackend-79of.onrender.com/api/posts" + search);
       setPosts(res.data);
+      setLoading(true)
     };
     fetchPosts();
   }, [search]);
@@ -21,8 +23,17 @@ export default function Home() {
     <>
       <Header />
       <div className="home">
+      {
+        loading ? (
         <Posts posts={posts} />
-        <Sidebar />
+
+      )
+      :(
+        <div className="Loading">Loading Please Wait.......</div>
+      )
+
+      }
+       <Sidebar />
       </div>
     </>
   );
